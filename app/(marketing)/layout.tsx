6f7ad3b4012414750d567e63e2b0f322bac8fec8
AneeshaRama <aneesharama@gmail.com>
2024-01-31 17:18:09 +0530

    Initial landing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './_components/Navbar'
+
+interface MarketingLayoutProps {
+    children: React.ReactNode
+}
+
+const MarketingLayout = ({children}: MarketingLayoutProps) => {
+  return (
+    <div className='h-full'>
+        <Navbar/>
+        <main className='h-full pt-40'>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default MarketingLayout
