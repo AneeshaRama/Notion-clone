a2e741a79bd810853dbd21079b22ffceee73079a
AneeshaRama <aneesharama@gmail.com>
2024-02-01 00:52:00 +0530

    added document schema and create api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,22 @@ import React from 'react'
 import { useUser } from '@clerk/clerk-react'
 import { Button } from '@/components/ui/button'
 import { PlusCircle } from 'lucide-react'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { toast } from 'sonner'
 
 const DocumentsPage = () => {    
   const {user} = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = ()=>{
+    const promise = create({title: "untitled"});
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note"
+    })
+  }
   return (
     <div className='h-full flex-col flex items-center justify-center space-y-4'>
       <Image
@@ -25,7 +38,7 @@ const DocumentsPage = () => {
       />
       <h2 className='text-lg font-medium'>Welcome to {user?.firstName}&apos;s Notion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className='h-4 w-4 mr-2'/>
         Create a note
       </Button>
