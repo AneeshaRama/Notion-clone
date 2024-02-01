b941aff62615387c0a302f73802f152f4d7148e7
AneeshaRama <aneesharama@gmail.com>
2024-02-01 22:41:21 +0530

    Added delete functionality

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -12,6 +12,7 @@ import { api } from '@/convex/_generated/api'
 import { toast } from 'sonner'
 import DocumentList from './DocumentList'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import TrashBox from './TrashBox'
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -147,7 +148,7 @@ const Navigation = () => {
                 <Item label="Trash" icon={Trash}/>
               </PopoverTrigger>
               <PopoverContent className='p-0 w-72' side={isMobile ? "bottom" : "right"}>
-                  <p>trash box</p>
+                  <TrashBox/>
               </PopoverContent>
             </Popover>
         </div>
