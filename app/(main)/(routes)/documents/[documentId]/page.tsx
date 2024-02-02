541e863b8c9fe482b2ad7d55b85122b553e42c50
AneeshaRama <aneesharama@gmail.com>
2024-02-02 13:28:47 +0530

    Added tool bar and icon picker

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,38 @@
+"use client"
+
+import Toolbar from '@/components/Toolbar'
+import { api } from '@/convex/_generated/api'
+import { Id } from '@/convex/_generated/dataModel'
+import { useQuery } from 'convex/react'
 import React from 'react'
 
-const DocumentIdPage = () => {
+interface DocumentIdPageProps{
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({params}: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  })
+
+  if(document === undefined){
+    return <div>
+      Loading...
+    </div>
+  }
+
+  if(document === null){
+    return <div>Not found</div>
+  }
   return (
-    <div>DocumentIdPage</div>
+    <div className='pb-40'>
+        <div className="h-[35vh]"/>
+      <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
+        <Toolbar initialData={document}/>
+      </div>
+    </div>
   )
 }
 
