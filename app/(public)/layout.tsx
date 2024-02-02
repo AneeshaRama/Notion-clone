5952a56d7e8a7e7beb9b029dfa1c4a0f87a88792
AneeshaRama <aneesharama@gmail.com>
2024-02-02 16:42:13 +0530

    Bug fix

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -4,7 +4,7 @@ const PublicLayout = ({
     children: React.ReactNode;
   }) => {
     return ( 
-      <div className="h-full dark:bg-[#1F1F1F]">
+      <div className="min-h-full dark:bg-[#1F1F1F]">
         {children}
       </div>
      );
